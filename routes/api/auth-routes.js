72c2d3b5bd6e83c00230fb3dffa7c7db1e27898e
vsC0dex <max071010@gmail.com>
2023-04-27 19:01:26 +0300

    done

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/auth-controller");
 
 const { validateBody } = require("../../utils");
 
-const { authenticate } = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -17,5 +17,12 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
